7959985760b490147da4e3b8334449f3d00c77cf
zhangolve <zhangolve@gmail.com>
2021-07-12 15:22:11 +0800

    update vue

--- tipranks/advanced/vue.config.js
+++ tipranks/advanced/vue.config.js
@@ -4,5 +4,13 @@ module.exports = {
          proxy: { 
              '/api': { target: 'https://www.tipranks.com', changeOrigin: true, ws: true, pathRewrite: { '^/api':  '/api', } } 
             }
-    }
+    },
+    chainWebpack: config => {
+        config
+        .plugin('html')
+        .tap(args => {
+          args[0].title = 'tipranks details'
+          return args
+        })
+      }
   }
